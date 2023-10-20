83845335d237935e47d7b1cb5778990db1a23429
Bart C. <bartcarlson.b.c@gmail.com>
2023-10-19 20:14:45 -0700

    refactor into less files. extract config to a yaml file

--- src/gen_spreedsheet.py
+++ src/gen_spreedsheet.py
@@ -1,9 +1,86 @@
 import pandas as pd
 import geopandas as gpd
-import extract_raster_var_by_strata as VarExtract
+import yaml
+from osgeo import gdal, ogr
+import numpy as np
+
+gdal.UseExceptions()
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
 
 
-def generate_spreedsheet(rasterfile, shapefile, spreedsheetpath):
+def var_by_strata():
+    """
+    takes a rasterfile of tree volumes and a shapefile of strata and returns a dataframe
+    with the standard deviation of the volumes in each strata.
+    the polygon can contain more than one occurrence of a strata, but they will be reported as one.
+    """
+
+    # Load the raster file
+    raster = gdal.Open(cfg["paths"]["rasterfile"])
+    rb = raster.GetRasterBand(cfg["raster_band"])
+
+    # Load the shapefile
+    shape = gpd.read_file(cfg["paths"]["shapefile"])
+
+    # Dissolve/merge polygons by the Strata_Dat column
+    merged_shape = shape.dissolve(by=cfg["shapefile_field_names"]["strata"])
+    merged_shape = merged_shape.reset_index()
+    merged_shape = merged_shape.rename(columns={"index": "Strata_Dat"})
+    # Create an empty list to store standard deviations
+    std_devs = []
+    # Get spatial reference from shapefile
+    srs = ogr.osr.SpatialReference()
+    srs.ImportFromWkt(shape.crs.to_wkt())
+    # Loop over each merged feature in shapefile
+    for geometry, data in zip(merged_shape.geometry, merged_shape.iterrows()):
+        # Convert shapely geometry to ogr geometry
+        ogr_geom = ogr.CreateGeometryFromWkt(geometry.wkt)
+
+        # Create an in-memory vector layer
+        mem_ds = ogr.GetDriverByName("Memory").CreateDataSource("temp")
+        layer = mem_ds.CreateLayer("temp", srs=srs, geom_type=ogr.wkbPolygon)
+
+        # Add the ogr_geom geometry to this layer
+        featureDefn = layer.GetLayerDefn()
+        feature = ogr.Feature(featureDefn)
+        feature.SetGeometry(ogr_geom)
+        layer.CreateFeature(feature)
+
+        # Use GDAL's RasterizeLayer to get the raster values for this geometry
+        mem_drv = gdal.GetDriverByName("MEM")
+        target_ds = mem_drv.Create(
+            "", raster.RasterXSize, raster.RasterYSize, 1, gdal.GDT_Byte
+        )
+        target_ds.SetProjection(raster.GetProjection())
+        target_ds.SetGeoTransform(raster.GetGeoTransform())
+
+        # Rasterize using the in-memory layer
+        gdal.RasterizeLayer(target_ds, [1], layer, burn_values=[1])
+
+        array = target_ds.ReadAsArray()
+
+        # Use np.where to get the raster values for this polygon
+        values = rb.ReadAsArray()[np.where(array == 1)]
+        values = values.astype(np.float64)
+        values = values[np.isfinite(values)]
+        nodata_value = rb.GetNoDataValue()
+        if nodata_value is not None:
+            values = values[values != nodata_value]
+
+        # Calculate standard deviation
+        # if len(values) > 0:
+        std_dev = np.std(values)
+        std_devs.append([data[1]["Strata_Dat"], std_dev])
+
+    # Convert list to DataFrame
+    df = pd.DataFrame(std_devs, columns=["Strata", "StandardDev"])
+
+    return df
+
+
+def generate_spreedsheet():
     """
     creates a spreedsheet for data entry.
     one the first page is a field to list the total number of plots.
@@ -18,18 +95,14 @@ def generate_spreedsheet(rasterfile, shapefile, spreedsheetpath):
     df_page1 = pd.DataFrame()
     df_page1["Total_Plots"] = [0]
 
-    df_page2 = VarExtract.var_by_strata(rasterfile, shapefile)
+    df_page2 = var_by_strata()
     df_page2["hard_set_plot number"] = 0
     df_page2["weight"] = 1.0
 
-    with pd.ExcelWriter(spreedsheetpath, engine="openpyxl") as writer:
+    with pd.ExcelWriter(cfg["paths"]["spreedsheet"], engine="openpyxl") as writer:
         df_page1.to_excel(writer, sheet_name="page1", index=False)
         df_page2.to_excel(writer, sheet_name="page2", index=False)
 
 
 if __name__ == "__main__":
-    # Paths to files
-    rasterfile = "../data/full_volume.tif"
-    shapefile = "../data/SVR_TimberStrata.shp"
-    spreedsheetpath = "../user_input/strata.xlsx"
-    generate_spreedsheet(rasterfile, shapefile, spreedsheetpath)
+    generate_spreedsheet()
